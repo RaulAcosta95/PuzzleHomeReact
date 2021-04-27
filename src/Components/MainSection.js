f45d96ad72c8e64723221a89fe01f44937887d1b
Raúl <raulacostaservicio@hotmail.com>
2021-04-26 20:11:51 -0500

    Estilos y grid responsive de MainSection

--- src/Components/MainSection.js
+++ src/Components/MainSection.js
@@ -2,21 +2,22 @@ import React from 'react';
 import IconoBusqueda from '../Images/Icono de busqueda naranja.png';
 import DosPersonasImagen from '../Images/Imagen dos personas dandose la mano.png';
 
+//Estilos
+import './css/MainSection.css';
+import './css/MainSectionGrid.css';
 function MainSection(props) 
 {  
     //Permite que funcione el switch de rutas
     return (
         <React.Fragment>
-            <section className="main">
-                <div className="info">
-                    <p className="innovate"><b>INNOVATIVE SOLUTION FOR SOURCING CANDIDATES</b></p>
-                    <p className="helpUsFind">Help us find <b>the missing puzzle, <br/> get rewarded</b> properly.</p>
-                    <form>
-                        <input type="text" placeholder="Keyword, job title or company"/>
-                        <button type="submit"><img className="LogoBusqueda" src={IconoBusqueda} alt="Icono de busqueda naranja"/></button>
+            <section className="Main">
+                    <p className="PrimerP"><b>INNOVATIVE SOLUTION FOR SOURCING CANDIDATES</b></p>
+                    <p className="SegundoP">Help us find <b>the missing puzzle, <br/> get rewarded</b> properly.</p>
+                    <form className="Form">
+                        <input className="FormInput" type="text" placeholder="Keyword, job title or company"/>
+                        <button className="FormButton" type="submit"><img className="LogoBusqueda" src={IconoBusqueda} alt="Icono de busqueda naranja"/></button>
                     </form>
-                </div>
-                <img className="DosPersonasDandoseLaMano" src={DosPersonasImagen} alt="Imagen dos personas dandose la mano, representa un acuerdo, hay dinero, un contrato de fondo y una lupa en el contrato que representa detalles"/>
+                    <img className="MainImagen" src={DosPersonasImagen} alt="Imagen dos personas dandose la mano, representa un acuerdo, hay dinero, un contrato de fondo y una lupa en el contrato que representa detalles"/>
             </section>
 
         </React.Fragment>
