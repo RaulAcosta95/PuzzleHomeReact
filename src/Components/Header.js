bf3601b8636329e5209421a1a0f8e9d3f4eaed22
Raúl <raulacostaservicio@hotmail.com>
2021-05-07 12:51:24 -0500

    menú desplegable y bordes botones

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,6 +11,22 @@ function Header(props)
         <React.Fragment>
             <header className="Header">
                 <img className="PiezaRompecabezas" src={PiezaRompecabezas} alt="Pieza de rompecabezas tipo portafolio"/>
+                <button class="BotonMenúDesplegable">
+                    {/* <i class="">---</i> */}
+                </button>
+        {/* <!-- Los links --> */}
+        {/* CAMBIAR A POR <LINK /> */}
+                <nav class="MenúDesplegable">
+                    <ul>
+                        {/* <li><p>Menú</p></li> */}
+                        <li><a className="Home2" href="">HOME</a></li>
+                        <li><a className="AboutUs2" href="">ABOUT US</a></li>
+                        <li><a className="Blog2" href="">BLOG</a></li>
+                        <li><a className="HowItWorks2" href="">HOW IT WORKS?</a></li>
+                        <button className="Boton11">LOG IN</button>
+                        <button className="Boton22">SING UP</button>
+                    </ul>
+                </nav>
                     <a className="Home" href="">HOME</a>
                     <a className="AboutUs" href="">ABOUT US</a>
                     <a className="Blog" href="">BLOG</a>
